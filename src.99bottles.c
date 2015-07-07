548473e637630bf49269d1103f1b1c79a4562def
UndergroundComputing <richard.kocjancic@gmail.com>
2015-07-07 15:27:11 -0400

    Added song lyrics.

--- src.99bottles.c
+++ src.99bottles.c
@@ -4,7 +4,7 @@ int main()
 {
 	int num_bottles=99;
 	for (;num_bottles>0;num_bottles--){
-		printf("%d\n",num_bottles);
+		printf("%d bottles of beer on the wall,\n%d bottles of beer.\nTake one down, pass it arround,\n%d bottles of beer on the wall.\n\n",num_bottles,num_bottles,num_bottles-1);
 	}
 	return 0;
 }
