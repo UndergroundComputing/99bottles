828289d090862655de910c9b31d35d0ff411a58e
UndergroundComputing <richard.kocjancic@gmail.com>
2015-07-07 22:04:21 -0400

    Corrected the conjugation of bottle/bottles.  I am not an English major!

--- src.99bottles.c
+++ src.99bottles.c
@@ -3,8 +3,10 @@
 int main()
 {
 	int num_bottles=99;
-	for (;num_bottles>0;num_bottles--){
+	for (;num_bottles>2;num_bottles--){
 		printf("%d bottles of beer on the wall,\n%d bottles of beer.\nTake one down, pass it arround,\n%d bottles of beer on the wall.\n\n",num_bottles,num_bottles,num_bottles-1);
 	}
+	printf("%d bottles of beer on the wall,\n%d bottles of beer.\nTake one down, pass it arround,\n%d bottle of beer on the wall.\n\n",num_bottles,num_bottles,num_bottles-1);
+	printf("%d bottle of beer on the wall,\n%d bottle of beer.\nTake one down, pass it arround,\n%d bottles of beer on the wall.\n\n",num_bottles-1,num_bottles-1,num_bottles-2);
 	return 0;
 }
